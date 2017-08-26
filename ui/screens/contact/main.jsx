9780e29a1dcfd3d5be12bd008af5aebb68593628
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2017-08-26 19:49:18 -0400

    Updated the contact section too

--- ui/screens/contact/main.jsx
+++ ui/screens/contact/main.jsx
@@ -6,7 +6,7 @@ export default class Contact extends React.Component {
     render() {
         return (
             <div className="contact">
-                <p>Liked my work? I am looking for summer UI/UX internship opportunities, let’s chat.</p>
+                <p>Liked my work? I am looking for full-time opportunities, let’s chat.</p>
                 <form method="POST" action="https://formspree.io/navdeepb@umich.edu" autoComplete="off">
                     <input type="hidden" name="_subject" value="Contact from portfolio - navdeepsb.com" />
                     <input type="email" name="_replyto" placeholder="Your email" spellCheck="false" required />
