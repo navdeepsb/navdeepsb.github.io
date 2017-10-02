d80aec2b8ef79ee2a24d2810c8e563663c9d082b
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2017-10-02 03:20:00 -0400

    Updated header line

--- ui/screens/contact/main.jsx
+++ ui/screens/contact/main.jsx
@@ -6,14 +6,14 @@ export default class Contact extends React.Component {
     render() {
         return (
             <div className="contact">
-                <p>Liked my work? I am looking for full-time opportunities, let’s chat.</p>
+                <p>Liked my work? Let’s chat.</p>
                 <form method="POST" action="https://formspree.io/navdeepb@umich.edu" autoComplete="off">
                     <input type="hidden" name="_subject" value="Contact from portfolio - navdeepsb.com" />
                     <input type="email" name="_replyto" placeholder="Your email" spellCheck="false" required />
                     <textarea name="message" placeholder="Your message" rows="6" spellCheck="false" required />
                     <button type="submit">Send</button>
                 </form>
-                <p>Or email me at navdeepb@umich.edu</p>
+                <p>Or email me at <strong>navdeepb@umich.edu</strong></p>
             </div>
         );
     }
