be673977c5e2d2f03ced1ebaad9b89d10b37a646
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2017-10-28 15:44:56 -0400

    Updated resume and curated atelier media backgrounds

--- ui/screens/resume/main.jsx
+++ ui/screens/resume/main.jsx
@@ -32,9 +32,9 @@ export default class Resume extends React.Component {
         return (
             <div className="resume">
                 <p className="pdf-text">
-                    <a href={ require( "./Resume - Navdeep Singh Bagga - UMSI 2018.pdf" ) } target="_blank" rel="nofollow" title="Resume PDF">Download PDF</a>
+                    <a href={ require( "./Resume - Navdeep Singh Bagga - UMSI 2018.pdf" ) } target="_blank" rel="nofollow" title="Download Resume as a PDF">Download Resume as a PDF</a>
                 </p>
-                { resumeEvents.map( ( event, idx ) => {
+                { resumeEvents.filter( item => !item.noShow ).map( ( event, idx ) => {
                     return <Event data={ event } key={ idx } />;
                 }) }
             </div>
